bdb44bbca025b60321660c056ce48144df9b5704
kiiaseilonen <kiia.seilonen@hotmail.com>
2024-12-04 14:48:44 +0200

    first commit

---
+++ app/app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = 5000;
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+// Määritä oikea hakemisto näkymille
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('login', { error: null });
+});
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    if (username === USERNAME && password === PASSWORD) {
+        res.render('success', { username });
+    } else {
+        res.render('login', { error: 'Invalid username or password' });
+    }
+});
+
+// Virheenkäsittely
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ test/tests/login_test.robot
@@ -0,0 +1,30 @@
+*** Settings ***
+Library    SeleniumLibrary
+Library    OperatingSystem
+
+*** Variables ***
+${URL}  http://host.docker.internal:5000
+${BROWSER}    Firefox
+${VALID_USERNAME}  %{USERNAME}
+${VALID_PASSWORD}  %{PASSWORD}
+${INVALID_USERNAME}  %{INVALID_USERNAME}
+${INVALID_PASSWORD}  %{INVALID_PASSWORD} 
+
+*** Test Cases ***
+Valid Login Test
+    
+    Open Browser  ${URL}  ${BROWSER}
+    Input Text  id=username  ${VALID_USERNAME}
+    Input Text  id=password  ${VALID_PASSWORD}
+    Click Button  //button[@type='submit']
+    Wait Until Page Contains Element    id=welcome-text
+    Title Should Be  Welcome
+    Close Browser
+
+Invalid Login Test
+    Open Browser  ${URL}  ${BROWSER}
+    Input Text  id=username  ${INVALID_USERNAME}
+    Input Text  id=password  ${INVALID_PASSWORD}
+    Click Button  //button[@type='submit']
+    Page Should Contain  Invalid username or password
+    Close Browser
